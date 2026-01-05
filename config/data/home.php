636b408b9550e81d8da0d62d50c30df1b2352dc2
Baboo81 <magicbaboo@gmail.com>
2026-01-05 20:24:10 +0100

    home.blade.php

--- config/data/home.php
+++ config/data/home.php
@@ -2,11 +2,15 @@
 
 return [
     'banner' => [
-        'main_Title' => "Informatio Libera",
+        'main_title' => "Informatio Libera",
         'subtitle' => [
             'line1' => "Des ressources pour les chercheurs de vérité.",
             'line2' => "De la géopolitique à la santé holistique, en passant par l'ingéniérie sociale.",
             'line3' => "Informatio Libera vous guidera vers une information libre, indépendante et affranchie de la doxa."
         ]
+    ],
+    'main_content' => [
+        'main_title' => "Que vous propose Informatio Libera ?",
+        'content' => "Informatio Libera a pour objectif d'apporter un nouvel angle de vue sur des thématiques qui sont de nos jours tronquées par les médias de masse. La sélection des différentes plateformes est basée sur des critères tels que ; l'honnêteté intellectuelle mais aussi sur l'intégrité de ces auteurs. Á l'heure actuelle les médias subventionnées ne jouissent plus de l'indépendance dont ils devraient être garants, d'où l'intérêt d'avoir mis sur pied ce site qui vous permettra de découvrir une série de plateforme qui oeuvre dans le sens de la souveraineté intellectuelle et du libre arbitre.",
     ],
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
+    {{-- Section : banner --}}
     <section class="banner">
         <div class="container">
             <div class="row d-flex justify-content-center align-items-center">
                 <div class="col-4">
                     <div class="box d-flex justify-content-center align-items-center rounded-5">
                         <h1 class="bannerTitle text-center">
-                            {{ $homeData['banner']['main_Title'] }}
+                            {{ $homeData['banner']['main_title'] ?? '' }}
                         </h1>
                     </div>
                 </div>
@@ -21,4 +22,27 @@
             </div>
         </div>
     </section>
+    {{-- Section : banner END --}}
+
+    {{-- Section : main-Content --}}
+    <section class="main-content">
+        <div class="container">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-8">
+                    <article class="quote">
+                        <div class="text-start">
+                            <img src="{{ asset('assets/img/svg/guillemetsO.svg') }}" alt="picto représentant des guillemets" class="quote-open">
+                        </div>
+                        <p class="text-center">
+                            {{ $homeData['main_content']['content'] ?? '' }}
+                        </p>
+                        <div class="text-end">
+                            <img src="{{ asset('assets/img/svg/guillemetsF.svg') }}" alt="picto représentant des guillemets" class="quote-close">
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- Section : main-Content END --}}
 </x-app-layout>
