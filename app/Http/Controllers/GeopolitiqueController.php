5c4d39231e066f271bc55b10d7cfb6a153a1ca29
Baboo81 <magicbaboo@gmail.com>
2026-01-09 10:23:02 +0100

    Created : component : card + controller

---
+++ app/Http/Controllers/GeopolitiqueController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GeopolitiqueController extends Controller
+{
+    public function show()
+    {
+        //Récupération des datas de geopolitique depuis config/data.geopolitique.php
+        $geopolitiqueData = config('data.geopolitique');
+
+        //On envoie à la vue
+        return view('geopolitique', compact('geopolitiqueData'));
+    }
+}
---
+++ app/View/Components/Card.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;//Import de la classe Component fournie par Laravel afin que les composants Blade en hérite
+
+//On déclare le composant Card
+class Card extends Component
+{
+    //Constructeur du composant, qui s'exècutera à chaque fois que l'on appelles <x-card> ds une view
+    public function __construct(
+        public string $title,//= <x-card title="titre">
+        public ?string $image = null,
+        public ?string $link = null,
+        public ?string $icon = null
+    ) {}//Constructeur abrégé, évite d'écrire : $this->title = $title
+
+    //Indique à Laravel quelle vue doit être rendue
+    public function render()
+    {
+        return view('components.card');
+    }
+}
