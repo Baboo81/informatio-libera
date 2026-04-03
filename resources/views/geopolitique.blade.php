17af6f4473ff3338d9c009a3f759664cb6149ea4
Baboo81 <magicbaboo@gmail.com>
2026-04-03 11:24:58 +0200

    sante.blade, css

--- resources/views/geopolitique.blade.php
+++ resources/views/geopolitique.blade.php
@@ -1,23 +1,17 @@
 <x-app-layout>
     <section class="geopolitique-content">
         <div class="container">
-            <div class="row justify-content-center">
-                <h2 class="text-center"> {{ $geopolitiqueData['main_title'] ?? '' }} </h2>
-                <article class="col-lg-8 col-md-10 col-12 text-center quote">
-                    <div class="text-start">
-                        <img src="{{ asset('assets/img/svg/guillemetsO.svg') }}" alt="picto représentant des guillemets"
-                            class="quote-open">
-                    </div>
-                    <p class="lead fst-italic">
-                        {{ $geopolitiqueData['intro'] ?? '' }}
-                    </p>
-                    <div class="text-end">
-                        <img src="{{ asset('assets/img/svg/guillemetsF.svg') }}" alt="picto représentant des guillemets"
-                            class="quote-close">
-                    </div>
-                </article>
+            <div class="black-box my-5">
+                <div class="row justify-content-center">
+                    <h2 class="text-center"> {{ $geopolitiqueData['main_title'] ?? '' }} </h2>
+                    <article class="col-lg-8 col-md-10 col-12 text-center quote">
+                        <p class="lead fst-italic">
+                            {{ $geopolitiqueData['intro'] ?? '' }}
+                        </p>
+                    </article>
+                </div>
             </div>
-            <div class="row justify-content-center">
+            <div class="row">
                 <div class="col-12 block-card">
                     @foreach ($themesData as $card)
                         <x-card :title="$card['title']" :sub-title="$card['sub_title'] ?? null" :image="$card['image']" :icon="$card['icon'] ?? null">
